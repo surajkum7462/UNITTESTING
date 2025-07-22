c0b1a56d3a6f7373e279c83e32a8b6002ebbf1d1
surajkumar <kumarsuraj7462998828@gmail.com>
2025-07-22 12:11:55 +0530

    Initial commit

---
+++ src/main/java/com/untit/testing/UnittestingApplication.java
@@ -0,0 +1,13 @@
+package com.untit.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnittestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnittestingApplication.class, args);
+	}
+
+}
